7fc1fb2984badeb25d043910a628860cb3ab2aa8
Marknjo <marknjo7@gmail.com>
2023-06-20 16:02:12 +0300

    ci(server): add not found exception handler to the app

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -10,7 +10,9 @@ import mediaRouter from '@routes/media'
 import reviewsRouter from '@routes/reviews'
 import toursRouter from '@routes/tours'
 import usersRouter from '@routes/users'
-import { EResStatus, JsonRes } from '@lib/types/JsonRes'
+import { EResStatus, TJsonRes } from '@lib/types/JsonRes'
+import { NotFoundException } from '@lib/exceptions/NotFoundException'
+import globalExceptionHandler from '@lib/middlewares/global-exception-handler'
 
 const app: Application = express()
 
@@ -26,7 +28,7 @@ app.use(express.json({ limit: '10kb' }))
 /// performance
 
 /// Routes
-app.get(`/api/v${+apiVersion}/_health`, (_req, res: JsonRes) => {
+app.get(`/api/v${+apiVersion}/_health`, (_req, res: TJsonRes) => {
   res.json({
     status: EResStatus.SUCCESS,
     data: {
@@ -41,5 +43,15 @@ app.use(baseUrl('media'), mediaRouter)
 app.use(baseUrl('reviews'), reviewsRouter)
 app.use(baseUrl('tours'), toursRouter)
 app.use(baseUrl('users'), usersRouter)
+
+/// Error handling
+// 404 response handler
+app.all('*', req => {
+  const message = `${req.originalUrl} cannot be found in this server.`
+  throw new NotFoundException(message)
+})
+
+/// global error handling
+app.use(globalExceptionHandler)
 
 export default app
---
+++ apps/server/src/lib/exceptions/NotFoundException.ts
@@ -0,0 +1,28 @@
+import {
+  EExceptionStatusCodes,
+  EResStatus,
+  TExceptionCollection,
+} from '@lib/types/JsonRes'
+import { HttpExceptionFilter } from './ExceptionHandler'
+
+export class NotFoundException extends HttpExceptionFilter {
+  statusCode = EExceptionStatusCodes.NOT_FOUND
+
+  constructor(public message: string = 'Not found') {
+    super(message)
+
+    Object.setPrototypeOf(this, NotFoundException.prototype)
+  }
+
+  serializeErrors(): TExceptionCollection {
+    return [
+      {
+        status: EResStatus.NOT_FOUND,
+        statusCode: this.statusCode,
+        data: {
+          message: this.message,
+        },
+      },
+    ]
+  }
+}
