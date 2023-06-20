3b967d479fd14dbb521e8e1ea0c599b99adfda27
Marknjo <marknjo7@gmail.com>
2023-06-20 15:59:19 +0300

    feat(server): add Express global exception handler

---
+++ apps/server/src/lib/middlewares/global-exception-handler.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from 'express'
+import { HttpExceptionFilter } from '../exceptions/ExceptionHandler'
+
+export default (
+  err: Error,
+  _req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (err instanceof HttpExceptionFilter) {
+    return res.status(err.statusCode).json({ errors: err.serializeErrors() })
+  }
+
+  res.status(500).json({
+    errors: [
+      {
+        message: 'Internal server error, Please try again',
+      },
+    ],
+  })
+
+  next()
+}
