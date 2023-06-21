eb5b87ade51102fc7944029fc4cd1cf8dd838390
Marknjo <marknjo7@gmail.com>
2023-06-21 19:07:19 +0300

    feat(server): add a generic get item by id factory function

---
+++ apps/server/src/lib/modules/getOne.ts
@@ -0,0 +1,58 @@
+import { asyncWrapper } from '@utils/handlerWrappers'
+import { NextFunction, Request, Response } from 'express'
+import { Query, Model as HandlerModel } from 'mongoose'
+import { IGetOneOptions, TGenericHandler } from './types'
+import { NotFoundException } from '@lib/exceptions/NotFoundException'
+
+/**
+ * Get One general handler method
+ * @param Model The model implementing helper handler
+ * @param options Passes filter options directly to the find, required modelName
+ * @returns Response or error message to the http request
+ */
+export function getOne<T>(
+  Model: Query<any, T> | HandlerModel<T>,
+  options: IGetOneOptions,
+): TGenericHandler {
+  return asyncWrapper(
+    async (req: Request, res: Response, next: NextFunction) => {
+      // setup id dynamically
+      let id
+
+      // Get Id -> currentId is set through the middleware - universal for any id holder
+      if (req.currentId) {
+        id = req.currentId
+      } else {
+        id = req.params[`${options.modelName}Id`] // i.e tourId, userId
+      }
+
+      // Prep Doc
+      let query
+
+      if (options.populateOptions) {
+        query = (Model as Query<any, T>)
+          .findById(id)
+          .populate(options.populateOptions)
+      } else {
+        query = (Model as Query<any, T>).findById(id)
+      }
+
+      // Find document by the id
+      const doc = await query
+
+      // Return error if there is no doc with the requested ID
+      if (!doc)
+        throw new NotFoundException(
+          `Could not find requested ${options.modelName}`,
+        )
+
+      // Return response
+      res.status(200).json({
+        status: 'success',
+        data: {
+          [options.modelName]: doc,
+        },
+      })
+    },
+  )
+}
