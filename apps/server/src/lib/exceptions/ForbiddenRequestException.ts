fc683aefe5afa1543a9cb8194533f60066310d0e
Marknjo <marknjo7@gmail.com>
2023-06-22 14:29:55 +0300

    ci(server): add a new custom error handler for handling 403 errors  - ForbiddenRequestException

---
+++ apps/server/src/lib/exceptions/ForbiddenRequestException.ts
@@ -0,0 +1,36 @@
+import {
+  EExceptionStatusCodes,
+  EResStatus,
+  TExceptionCollection,
+} from '@lib/types/JsonRes'
+import { HttpExceptionFilter } from './ExceptionHandler'
+
+export class ForbiddenRequestException extends HttpExceptionFilter {
+  statusCode = EExceptionStatusCodes.REQUEST_FORBIDDEN
+
+  constructor(
+    public message: string = 'Your request is denied check your credentials, please!',
+    code?: EExceptionStatusCodes,
+  ) {
+    super(message)
+
+    if (code) {
+      this.statusCode = code
+    }
+
+    Object.setPrototypeOf(this, ForbiddenRequestException.prototype)
+    Error.captureStackTrace(this, this.constructor)
+  }
+
+  serializeErrors(): TExceptionCollection {
+    return [
+      {
+        status: EResStatus.FAILURE,
+        statusCode: this.statusCode,
+        data: {
+          message: this.message,
+        },
+      },
+    ]
+  }
+}
