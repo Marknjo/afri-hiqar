6df5ffd6e4ff14749cb92fb893e408a209fc7185
Marknjo <marknjo7@gmail.com>
2023-06-23 18:51:07 +0300

    feat(server): and common JWT errors handlers in the globalErrorHandler

--- apps/server/src/lib/middlewares/global-exception-handler.ts
+++ apps/server/src/lib/middlewares/global-exception-handler.ts
@@ -6,7 +6,27 @@ import {
   IExceptionResponse,
   TExceptionCollection,
 } from '@lib/types/JsonRes'
-import { isProd } from '@utils/env'
+import { isDev, isProd } from '@utils/env'
+import { BadRequestException } from '@lib/exceptions/BadRequestException'
+
+/**
+ * Handles a broken JWT token
+ * @returns
+ */
+const jwtTokenMalformedHandler = () => {
+  // @TODO: A good place to implement serious login messages, and throttling and burning of the IP trying to access the resource.
+  const message = 'Please login with valid credentials to access the resource'
+  return new BadRequestException(message, 401)
+}
+
+/**
+ * Handle expired JWT token Error
+ * @returns Object of error message
+ */
+const jwtTokenExpiredHandler = () => {
+  const message = `Your login session has expired. Please login again to access the route.`
+  return new BadRequestException(message, 401)
+}
 
 export default (
   err: Error,
@@ -17,11 +37,22 @@ export default (
   >,
   next: NextFunction,
 ) => {
-  if (err instanceof HttpExceptionFilter) {
+  console.log({ error: err.name })
+
+  // Handler JWT Common errors
+  if (err.name === 'TokenExpiredError') err = jwtTokenExpiredHandler()
+  if (err.name === 'JsonWebTokenError') err = jwtTokenMalformedHandler()
+
+  /// handle all errors except 500 types of errors
+
+  if (
+    err instanceof HttpExceptionFilter &&
+    ((isProd && !String(err.statusCode).includes('5')) || isDev)
+  ) {
     return res.status(err.statusCode).json({ errors: err.serializeErrors() })
   }
 
-  /// production error message handling
+  /// production error generic message handler
   if (isProd) {
     res.status(500).json({
       errors: [
@@ -37,7 +68,7 @@ export default (
     return
   }
 
-  /// Dev error message handling
+  /// Handle all errors in production error message handling
   res.status(500).json({
     errors: [
       {
