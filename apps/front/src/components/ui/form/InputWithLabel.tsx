48a3b7bafacc511fe11ca51d5c12c0c81fc2dc7d
Marknjo <marknjo7@gmail.com>
2023-06-30 20:47:06 +0300

    style(@mj/front): add form structures

---
+++ apps/front/src/components/ui/form/InputWithLabel.tsx
@@ -0,0 +1,11 @@
+import { Input } from '@ui/form/Input'
+import { Label } from '@ui/form/Label'
+
+export function InputWithLabel() {
+  return (
+    <div className="grid w-full max-w-sm items-center gap-1.5">
+      <Label htmlFor="email">Email</Label>
+      <Input type="email" id="email" placeholder="Email" />
+    </div>
+  )
+}
---
+++ apps/front/src/components/ui/form/index.ts
@@ -0,0 +1,5 @@
+export * from './Input'
+export * from './Label'
+export * from './InputWithLabel'
+export * from './Form'
+export * from './InputForm'
