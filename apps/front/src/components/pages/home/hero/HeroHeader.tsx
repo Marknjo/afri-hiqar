273a6e2ed8b39a3d62497c439a0b5fa6395ce843
Marknjo <marknjo7@gmail.com>
2023-06-30 21:23:49 +0300

    style(@mj/front): add page hero Header section

--- apps/front/src/components/pages/home/hero/HeroHeader.tsx
+++ apps/front/src/components/pages/home/hero/HeroHeader.tsx
@@ -2,17 +2,21 @@ import { Heading1 } from '@ui/typography'
 
 export default function HeroHeader({ className }: { className?: string }) {
   return (
-    <header className={`${className}`}>
-      <Heading1>
-        <span>
-          <span>The</span>
-          <span>Worlds</span>
-          <span>Most</span>
+    <header
+      className={`col-start-2 col-span-12 row-start-1 row-span-2 items-center z-[2] place-self-center pb-8  ${className}`}
+    >
+      <Heading1 className="flex flex-col items-center text-primary-foreground uppercase">
+        <span className="flex flex-col items-center text-sm md:text-xl tracking-widest leading-none text-primary-indigo-2/90 py-2 px-4 mb-3 md:mb-5 border-b-4 md:border-b-8 border-dotted border-b-accent-gold-10 w-5 md:w-10">
+          <span className="">The</span>
+          <span className="">World's</span>
+          <span className="">Most</span>
         </span>
 
-        <span>
+        <span className="text-3xl md:text-5xl flex flex-col items-center tracking-widest border-b-4 md:border-b-8 border-dotted border-b-accent-gold-10 w-8 md:w-10 pb-2 md:pb-4">
           <span>Extra</span>
-          <span>Ordinary</span>
+          <span className="text-4xl md:text-6xl text-primary bg-primary-foreground/60 px-4 py-2 mb-2 mt-1 rounded-sm ">
+            Ordinary
+          </span>
           <span>Places</span>
         </span>
       </Heading1>
--- apps/front/src/components/pages/home/hero/index.tsx
+++ apps/front/src/components/pages/home/hero/index.tsx
@@ -4,6 +4,7 @@ import { GridSpacer } from '@ui/GridSpacer'
 /// Compose Hero Section
 
 import HeroImage from './HeroImage'
+import HeroHeader from './HeroHeader'
 
 export default function HomeHero({ className }: { className?: string }) {
   return (
@@ -11,6 +12,10 @@ export default function HomeHero({ className }: { className?: string }) {
       {/* Hero Image */}
       <div className="col-span-full row-start-1 row-span-2  z-[2] bg-gradient-to-tr from-primary-indigo-10/80 to-primary-indigo-1/5 " />
       <HeroImage />
+
+      {/* Hero Header */}
+      <div className="absolute top-0 left-0 z-[25] bg-slate-400" />
+      <HeroHeader />
 
       {/* SEARCH */}
 
